619e6f25ca2e567b780398fe38998477130ac796
Mohsen Hariri <mhariri68@gmail.com>
2024-08-11 11:36:23 -0400

    llama checked

--- llm/models/llama/README.md
+++ llm/models/llama/README.md
@@ -1,3 +1,9 @@
 # Llama
 
 [Meta-llama](https://github.com/meta-llama/llama/tree/main/llama), the code doesn't have consistency! it is working but a bit messy!
+
+
+## Llama models
+
+- Single node implementation: `llm/models/llama/model_single_node.py`
+
--- llm/models/llama/inference.py
+++ llm/models/llama/inference.py
@@ -4,56 +4,67 @@ from pathlib import Path
 import torch
 from Config import ModelArgs
 from init import init
-from model import Transformer
-from tokenizer import load_tokenizer
+from model_single_node import Llama2
+from text_generation import generate
+from tokenizer import Tokenizer
 
 config = init()
 
-tokenizer, vocab_size = load_tokenizer()
+tokenizer = Tokenizer(f"{config.base_dir}/.cache/meta_llama2/tokenizer.model")
 
 
-def load_model(llama_path=Path(f"{config.base_dir}/.cache/meta_llama2/llama-2-7b/")):
-
-    max_seq_len = 20  # To be fixed
-    max_batch_size = 5  # To be fixed
-
-    ( 
-        torch.set_default_dtype(torch.float16)
-        if config.device.type == "cuda"
-        else torch.set_default_dtype(torch.bfloat16)
-    ) # https://pytorch.org/docs/stable/generated/torch.set_default_dtype.html#torch.set_default_dtype
-
-    checkpoints_path = sorted(
-        (llama_path).glob("*.pth")
-    )  # For llama-2-7b, there is no need to sort the checkpoints since there is only one checkpoint.
-
-    assert len(checkpoints_path) > 0, f"No checkpoints found in {checkpoints_path}"
-
-    checkpoint = torch.load(
-        checkpoints_path[0], map_location="cpu"
-    )  # Load the checkpoint on CPU, [0] since there is only one checkpoint
-    # Comment from Meta repo: The only unmatched key in the checkpoint is rope.freqs. Remove it
-    del checkpoint["rope.freqs"]  # Remove the unmatched key
-
+def load_config(
+    vocab_size,
+    llama_path=Path(f"{config.base_dir}/.cache/meta_llama2/llama-2-7b/"),
+):
     with open(llama_path / "params.json", "r") as f:  # Load the params
         params = json.loads(f.read())
 
     model_args = ModelArgs(
-        max_seq_len=max_seq_len,
-        max_batch_size=max_batch_size,
-        device=config.device,
+        device="cuda",
         **params,
     )
     model_args.vocab_size = vocab_size
 
-    model = Transformer(model_args).to(config.device)
-
-    model.load_state_dict(checkpoint, strict=True)
-
-    print("Checkpoint loaded successfully")
-
-    return model, model_args
+    return model_args
 
 
+model_args = load_config(tokenizer.vocab_size)
 
-model, model_args = load_model()
+model = Llama2.from_pretrained(
+    weight_path=Path(f"{config.base_dir}/.cache/meta_llama2/llama-2-7b/"),
+    params=model_args,
+)
+
+torch.cuda.manual_seed(42)
+torch.manual_seed(42)
+
+prompt = ["Hello, I'm a language model", "Simply put, I'm a text generation model"]
+
+input_ids, input_masks, total_len, min_prompt_len = tokenizer.encode_prompts(
+    prompts=prompt, max_gen_len=128, params=model.params, device=config.device
+)
+
+output_ids = generate(
+    model=model,
+    input_ids=input_ids,
+    input_masks=input_masks,
+    min_prompt_len=min_prompt_len,
+    total_len=total_len,
+    eos_token=tokenizer.eos_token,
+    # method="greedy",
+    method="top_p",
+    # method="top_k",
+    temperature=0.6,
+    top_k=50,
+    top_p=0.8,
+    max_length=60,
+    max_new_tokens=None,
+    num_return_sequences=1,
+)
+
+decoded_output = tokenizer.decode_tokens(output_ids)
+
+for text in decoded_output:
+    print("-" * 50, "\n")
+    print(text)
